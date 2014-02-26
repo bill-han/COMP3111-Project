9872dd8341322db9b0e5e09d3484ac413a8aa69a
Shuo Han <han@dmk106.resnet.ust.hk>
2014-02-27 02:10:55 +0800

    AddDrawer

--- Not3r!/gen/com/example/not3r/R.java
+++ Not3r!/gen/com/example/not3r/R.java
@@ -21,16 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int Item=0x7f080002;
-        public static final int NoteList=0x7f080001;
-        public static final int NoteSearch=0x7f080000;
-        public static final int action_settings=0x7f080003;
+        public static final int Item=0x7f080004;
+        public static final int action_settings=0x7f080005;
+        public static final int drawer_layout=0x7f080000;
+        public static final int left_drawer=0x7f080003;
+        public static final int note_list=0x7f080002;
+        public static final int note_search=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_note_list=0x7f030000;
+        public static final int drawer=0x7f030000;
         public static final int list_item=0x7f030001;
     }
     public static final class menu {
@@ -39,6 +43,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050004;
+        public static final int drawer_open=0x7f050003;
         public static final int hint=0x7f050002;
     }
     public static final class style {
--- Not3r!/src/com/example/not3r/NoteList.java
+++ Not3r!/src/com/example/not3r/NoteList.java
@@ -1,17 +1,61 @@
 package com.example.not3r;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class NoteList extends Activity {
 
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_note_list);
+		setContentView(R.layout.drawer);
+		mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+		mDrawerToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.drawable.ic_drawer,R.string.drawer_open,R.string.drawer_close);
+		// Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		String[] show={"abc","def","ghi"};
+		ListView noteList=(ListView)findViewById(R.id.note_list);
+		noteList.setAdapter(new ArrayAdapter<String>(this,R.layout.list_item,show));
 	}
 
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
