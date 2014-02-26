ef85e27bed2379378346d040d2703cfb87bafe39
Shuo Han <han@wf109-087.ust.hk>
2014-02-26 19:06:09 +0800

    SearchView

--- Not3r!/gen/com/example/not3r/R.java
+++ Not3r!/gen/com/example/not3r/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Item=0x7f080001;
-        public static final int NoteList=0x7f080000;
-        public static final int action_settings=0x7f080002;
+        public static final int Item=0x7f080002;
+        public static final int NoteList=0x7f080001;
+        public static final int NoteSearch=0x7f080000;
+        public static final int action_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_note_list=0x7f030000;
