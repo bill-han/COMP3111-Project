54065604cd0f414f10eef4290e59178526297b2f
Shuo Han <han@dmk106.resnet.ust.hk>
2014-04-13 22:57:58 +0800

    Revert 4b6dba4..d68eaa9
    This rolls back to commit 4b6dba4d2059766c11800a3701d62ab95f4b1910.

--- Not3rTest/src/com/example/not3r/test/Not3rDBTest.java
+++ Not3rTest/src/com/example/not3r/test/Not3rDBTest.java
@@ -1,35 +1,18 @@
 package com.example.not3r.test;
 
 import android.test.AndroidTestCase;
-<<<<<<< HEAD
 
 import com.example.not3r.Not3rDB;
-=======
-import com.example.not3r.NoteDatabase;
-import android.util.Log;
-import java.util.List;
-import java.util.Map;
->>>>>>> FETCH_HEAD
 
 public class Not3rDBTest extends AndroidTestCase {
 
 	public Not3rDBTest() {
 		// TODO Auto-generated constructor stub
 	}
-<<<<<<< HEAD
 
 	public void createDB() {
 		Not3rDB helper = new Not3rDB(getContext());
 		helper.getWritableDatabase();
 	}
-=======
-	
-	public void createDB() {
-		NoteDatabase helper = new NoteDatabase(getContext());
-		helper.getWritableDatabase();
-	}
-	
-
->>>>>>> FETCH_HEAD
 
 }
