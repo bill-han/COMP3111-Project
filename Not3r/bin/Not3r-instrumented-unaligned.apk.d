60e43cd8cb3f727b499899ba7ec3d1582a1ffd17
bill-han <bill.han1992@gmail.com>
2014-04-30 22:00:01 +0800

    Maybe fix the failure on Jenkins CI

---
+++ Not3r/bin/Not3r-instrumented-unaligned.apk.d
@@ -0,0 +1,6 @@
+/Users/han/Documents/COMP3111-Project/Not3r/bin/Not3r-instrumented-unaligned.apk : \
+/Users/han/Documents/COMP3111-Project/Not3r/bin/Not3r.ap_ \
+/Users/han/Documents/COMP3111-Project/Not3r/bin/classes.dex \
+/Users/han/Documents/COMP3111-Project/Not3r/libs/android-support-v4.jar \
+/Users/han/Documents/COMP3111-Project/Not3r/libs/android-support-v7-appcompat.jar \
+/Applications/adt-bundle-mac-x86_64-20140321/sdk/tools/lib/emma_device.jar \
---
+++ Not3rTest/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/han/Documents/COMP3111-Project/Not3rTest/bin/AndroidManifest.xml : \
+/Users/han/Documents/COMP3111-Project/Not3rTest/AndroidManifest.xml \
